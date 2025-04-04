2efbeb4630ef7e58e770fb956564994d11cd0d2a
Dillon Chapuli <148895731+dillonpachuli@users.noreply.github.com>
2025-04-04 16:34:12 +0000

    first 4.4.25

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        Textbook bio2015 = new Textbook("Biology", 49.75, 2);
+        Textbook bio2019 = new Textbook("Biology", 39.75, 3);
+        bio2019.getEdition();
+        bio2019.getBookInfo();
+    }
+}
---
+++ Textbook.java
@@ -0,0 +1,16 @@
+public class Textbook extends Book{
+    private int edition;
+
+    public Textbook(String title, double price, int edition){
+        super(title, price);
+        this.edition = edition;
+    }
+
+    public int getEdition(){
+        return edition;
+    }
+
+    public String getBookInfo(){
+        return super.getBookInfo() + "-" + edition;
+    }
+}
